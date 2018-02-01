a19facba977b007c1276d0e36a5b9a8078ae358b
Leandro Ramos <leandroramos@usp.br>
2018-02-01 16:25:12 -0200

    Issue6 - Confirmar a ação de delete

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -36,8 +36,16 @@
     <!-- Placed at the end of the document so the pages load faster -->
    
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+  <script type="text/javascript">
+     $(function () {
+        $(".delete-item").on("click", function(){
+            return confirm("Tem certeza?");
+        });
+    });
+  </script>
 
   </body>
 </html>
