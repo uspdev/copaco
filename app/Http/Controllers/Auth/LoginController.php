9734c15cdb6f8a38ee3b8c6eba7a4a101bb61bd1
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2018-04-04 11:24:35 -0300

    Instalada a biblioteca uspdev/senhaunica-socialite
    Adicionei em .env .env.example as credenciais do provider criado para o senha única
    No LoginController:
    - Adicionei o método logout
    - Adicionei a restrição por admins
    - Adicionei a atualização de dados
    - Adicionei o redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use App\User;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,4 +39,56 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+ 	
+	public function redirectToProvider()
+    {
+        return Socialite::driver('senhaunica')->redirect();
+    }
+    
+	public function handleProviderCallback()
+	{
+        $userSenhaUnica = Socialite::driver('senhaunica')->user();
+        
+        // aqui vc pode inserir o usuário no banco de dados local, fazer o login etc.
+        
+		# busca o usuário local
+        $user = User::find($userSenhaUnica->id);
+        
+		# restrição só para admins
+        $admins = explode(',', trim(env('CODPES_ADMINS')));
+        
+		if (!in_array($userSenhaUnica->id, $admins)) {
+            # exibir mensagem flash de restrição...
+            dd('ACESSO RESTRITO!');
+            return redirect('/');
+        }    
+        
+		# se o usuário local NÃO EXISTE, cadastra
+        if (is_null($user)) {
+            $user = new User;
+            $user->id = $userSenhaUnica->id;
+            $user->email = $userSenhaUnica->email;
+            $user->name = $userSenhaUnica->name;
+            $user->save();
+        } else {
+            # se o usuário EXISTE local
+            # atualiza os dados
+            $user->id = $userSenhaUnica->id;
+            $user->email = $userSenhaUnica->email;
+            $user->name = $userSenhaUnica->name;
+            $user->save(); 
+        }
+        
+		# faz login
+        Auth::login($user, true);
+        
+		# redireciona
+        return redirect('/');  
+    }
+    
+	public function logout() {
+        Auth::logout();
+        return redirect('/');
+    }
+    
 }
