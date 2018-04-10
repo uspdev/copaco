29fc315eb95db2552e2729c7b58bca350a942771
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-04-09 22:26:36 -0300

    Validação do patrimônio usando uspdev/patrimonio

---
+++ app/Rules/Patrimonio.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Uspdev\dadosUsp;
+
+class Patrimonio implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $patrimonio = new dadosUsp;
+        $xml = $patrimonio->fetchNumpat($value);
+        if (strpos($xml,'The server encountered an unexpected condition') !== false) {
+            return false;
+        }
+        // talvez os dados desse array em alguma coisa...
+        $output = $patrimonio->xml2array($xml);
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Patrimonio nao valido';
+    }
+}
