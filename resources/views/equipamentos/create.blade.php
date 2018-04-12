00b08ece4115b55594fc599ffc44f118a49ce5e0
André Luiz Girol <andregirol@gmail.com>
2018-04-12 16:12:10 -0300

    AdminLTE em equipamentos/create.blade.php

--- resources/views/equipamentos/create.blade.php
+++ resources/views/equipamentos/create.blade.php
@@ -1,19 +1,28 @@
-@extends('dashboard.master')
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Cadastrar Equipamento</h1>
+@stop
 
 @section('content')
-<h1>Cadastrar Equipamento</h1>
+    <div class="row">
+        @include('messages.flash')
+        @include('messages.errors')
 
-@include('messages.flash')
-@include('messages.errors')
-
-<div class="row">
-    <div class="col-lg-4">
-        <form action="{{ url('equipamentos') }}" method="post">
-        {{ csrf_field() }}
-        @include('equipamentos.form')
-        </form>
+        <div class="col-md-6">
+            <form action="{{ url('equipamentos') }}" method="post">
+            {{ csrf_field() }}
+            @include('equipamentos.form')
+            </form>
+        </div>
     </div>
-</div>
+@stop
 
-
-@endsection
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.pt-BR.min.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+@stop
