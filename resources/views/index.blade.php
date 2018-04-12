8329bab18b415c2c80615b3ab413b89139d2785c
André Luiz Girol <andregirol@gmail.com>
2018-04-12 14:59:33 -0300

    Customizando index.blade.php para AdminLTE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,27 @@
-@extends ('dashboard.master')
+@extends('adminlte::page')
 
-@section ('content')
+@section('title', 'USPdev - COPACO')
 
-@include ('messages.flash')
-@include ('messages.errors')
+@section('content_header')
+    <h1>Sistema COntrole do PArque COmputacional</h1>
+@stop
 
-<h1>Sistema COntrole do PArque COmputacional</h1>
-
-    @auth
-        <h3><b>Olá {{ Auth::user()->name }},</b></h3>
-        Acesse as opções no menu ao lado
-    @else
-        Você ainda não fez seu login com a senha única USP <a href="/login"> Faça seu Login! </a>
-    @endauth
-    <h3>Com o Copaco você pode</h3>
-    <ul>
-        <li>Cadastrar computadores, APs, switches </li>
-        <li>Cadastrar rede...</li>
-        <li>...</li>
-    </ul>
-
-@endsection
+@section('content')
+    
+    @include ('messages.flash')
+    @include ('messages.errors')
+    
+        @auth
+            <h3><b>Olá {{ Auth::user()->name }},</b></h3>
+            Acesse as opções no menu ao lado
+        @else
+            Você ainda não fez seu login com a senha única USP <a href="/login"> Faça seu Login! </a>
+        @endauth
+        <h3>Com o Copaco você pode</h3>
+        <ul>
+            <li>Cadastrar computadores, APs, switches </li>
+            <li>Cadastrar rede...</li>
+            <li>...</li>
+        </ul>
+    
+@stop
