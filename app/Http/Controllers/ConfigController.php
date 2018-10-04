590f59b2f628d45069ae8fad30ba4c323108a437
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-10-04 11:25:24 -0300

    iniciando config table

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Config;
 
 class ConfigController extends Controller
 {
@@ -15,5 +16,12 @@ class ConfigController extends Controller
     {
         $consumer_deploy_key = env('CONSUMER_DEPLOY_KEY');
         return view('config/index',compact('consumer_deploy_key'));
+    }
+
+    public function config(Request $request)
+    {
+        dd($request->key);
+
+        //return redirect("/config");
     }
 }
---
+++ database/migrations/2018_10_04_141540_create_configs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('key');
+            $table->longText('value')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configs');
+    }
+}
