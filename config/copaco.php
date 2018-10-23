6d7196469c195795dc1eda5dca6ebf4bd55d7234
André Luiz Girol <andregirol@gmail.com>
2018-10-23 19:52:45 -0300

    Criado arquivo principal de configuração

---
+++ config/copaco.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | USPdev Copaco - Arquivo Principal de Configuração
+    |--------------------------------------------------------------------------
+    |
+    | Este arquivo abriga as integrações com bibliotecas desenvolvidas pelo USPdev e bibliotecas
+    | externas. Novas diretivas devem ser criadas aqui se a release do desenvolvedor
+    | não provê um arquivo próprio de configuração
+    |
+    */
+]
