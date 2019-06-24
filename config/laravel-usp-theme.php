fbc0e2ab92a000253ef3e9a71fa22392bc6b66fa
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-06-24 14:33:50 -0300

    usp theme adicionado

---
+++ config/laravel-usp-theme.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'title'=> 'USPdev',
+    'dashboard_url' => '/',
+    'logout_method' => 'POST',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+    'menu' => [
+        [
+            'text' => 'Minha Conta',
+            'url'  => '/',
+            'can'  => 'equipamentos.create',
+        ],
+        [
+            'text' => 'Equipamentos',
+            'url'  => '/equipamentos',
+            'icon' => 'desktop',
+            'can'  => 'equipamentos.create',
+        ],
+        [
+            'text'        => 'Redes',
+            'url'         => '/redes',
+            'icon'        => 'sitemap',
+            'can'         => 'admin',
+        ],
+        [
+            'text'        => 'Configurações',
+            'url'         => '/config',
+            'icon'        => 'file',
+            'can'         => 'admin',
+        ],
+        [
+            'text'        => 'Grupos',
+            'url'         => '/roles',
+            'icon'        => 'group',
+            'can'         => 'admin',
+        ], 
+        [
+            'text'        => 'Pessoas',
+            'url'         => '/users',
+            'icon'        => 'home',
+            'can'         => 'admin',
+        ], 
+    ],
+];
