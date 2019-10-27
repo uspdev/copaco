6878349a7b22aef8fef8d2f110740c518affdf51
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-10-27 16:56:26 -0300

    update composer

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
 
         /*
          * Package Service Providers...
