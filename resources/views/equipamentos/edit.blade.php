e6ac71f77865fb319027c57ebda8b63286110591
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-03-28 16:40:01 -0300

    Fix position flash messages

--- resources/views/equipamentos/edit.blade.php
+++ resources/views/equipamentos/edit.blade.php
@@ -5,10 +5,10 @@
 @stop
 
 @section('content')
-    <div class="row">
     @include('messages.flash')
     @include('messages.errors')
-
+    
+    <div class="row">
 
     <div class="col-md-6">
         <form action="{{ url('equipamentos'). '/' . $equipamento->id }}" method='post'>
