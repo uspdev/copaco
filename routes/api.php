d23400c3838c8e70ca0b62d527c35c42b495f796
Pedro Luis <pedroluis.souzafilho@usp.br>
2026-06-15 16:37:23 -0300

    Inclusão do gerador do kea dhcp

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\Api\DhcpController;
 use App\Http\Controllers\Api\FreeradiusController;
 use App\Http\Controllers\Api\EquipamentoController;
+use App\Http\Controllers\Api\KeaController;
 
 Route::post('/dhcpd.conf', [DhcpController::class, 'dhcpd']);
 Route::post('/uniquedhcpd.conf', [DhcpController::class, 'uniquedhcpd']);
 Route::post('/freeradius/authorize_file', [FreeradiusController::class, 'authorize_file']);
+Route::post('/kea', [KeaController::class, 'kea']);
+Route::post('/kea/unique', [KeaController::class, 'uniquekeab']);
